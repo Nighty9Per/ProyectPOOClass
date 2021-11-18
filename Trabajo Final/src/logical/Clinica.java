7a359cb2cd355ea9aa1ef3c7dcf0600297f15e6c
reinoso <reinoso@DESKTOP-947B377>
2021-11-18 16:01:57 -0400

    Modificacion Clase Clinica:
    -Creacion de 3 clases nuevas

--- Trabajo Final/src/logical/Clinica.java
+++ Trabajo Final/src/logical/Clinica.java
@@ -1,5 +1,10 @@
 package logical;
 
+import java.util.ArrayList;
+
 public class Clinica {
+	private ArrayList<Paciente> misPacientes;
+	private ArrayList<Vacuna> misVacunas;
+	private ArrayList<CitaMedica> misCitas;
 	
 }
