df04ee0c2b33822e3720f174c105d3b68f20727c
reinoso <reinoso@DESKTOP-947B377>
2021-11-18 15:29:55 -0400

    Creacion de clase heredera U_Administrador

---
+++ Trabajo Final/src/logical/U_Administrador.java
@@ -0,0 +1,18 @@
+package logical;
+
+public class U_Administrador extends Usuario {
+
+	private String puestoLaboral;
+	public U_Administrador(String codigoUsuario, String cedula, String login, String password, String nombre,
+			String telefono, String direccion, String puestoLaboral) {
+		super(codigoUsuario, cedula, login, password, nombre, telefono, direccion);
+		this.puestoLaboral = puestoLaboral;
+	}
+	public String getPuestoLaboral() {
+		return puestoLaboral;
+	}
+	public void setPuestoLaboral(String puestoLaboral) {
+		this.puestoLaboral = puestoLaboral;
+	}
+
+}
