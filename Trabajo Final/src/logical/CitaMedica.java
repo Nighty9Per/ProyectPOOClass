9392b7d5fd9794eb4d01993ca44243ba23902e57
reinoso <reinoso@DESKTOP-947B377>
2021-11-18 15:03:32 -0400

    Creacion Clase CitaMedica

---
+++ Trabajo Final/src/logical/CitaMedica.java
@@ -0,0 +1,15 @@
+package logical;
+
+public class CitaMedica {
+
+	private String codigoCita;
+	private Date fechaCita;
+	private String nombrePaciente;
+	private telefonoPaciente;
+	private Medico medico;
+	private Boolean cancelada;
+	public CitaMedica() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
