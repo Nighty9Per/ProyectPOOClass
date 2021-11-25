a3eea008db2eb40378b7da3683c15938c762bf83
mijon <mijon@DESKTOP-71V7KSQ>
2021-11-25 15:02:16 -0400

    Cambio a PrincipalClinica y AccesoSistema

--- Trabajo Final/src/visual/PrincipalClinica.java
+++ Trabajo Final/src/visual/PrincipalClinica.java
@@ -3,6 +3,7 @@ package visual;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -22,12 +23,25 @@ import java.awt.event.ActionEvent;
 public class PrincipalClinica extends JFrame {
 
 	private JPanel contentPane;
-	private JMenuItem mntmNewMenuItem;
-	private JMenu mnNewMenu_1;
-	private JMenu mnNewMenu;
-	private JMenu mnNewMenu_3;
-	private JMenu mnNewMenu_4;
-	private JMenu mnNewMenu_2;
+	private JPanel panelMain;
+	private JPanel panelUserInfo;
+	private JButton btnLogOut;
+	private JPanel panelUser;
+	private JMenuItem mntmRegMedico;
+	private JMenu mnAdministrador;
+	private JMenu mnMedico;
+	private JMenu mnVacunas;
+	private JMenu mnListEnfermedades;
+	private JMenuItem mntmRegVacuna;
+	private JMenuItem mntmRegEnfermedad;
+	private JMenuItem mntmListCitaMedica;
+	private JMenuItem mntmCrearConsulta;
+	private JMenuItem mntmListVacuna;
+	private JMenuItem mntmNewMenuItem_6;
+	private JMenuBar menuBar;
+	private JPanel panelFoundation;
+	private JPanel panelLogin;
+	private JButton btnNewButton_1;
 
 	/**
 	 * Launch the application.
@@ -36,7 +50,7 @@ public class PrincipalClinica extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PrincipalClinica frame = new PrincipalClinica(null);
+					PrincipalClinica frame = new PrincipalClinica();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -47,66 +61,102 @@ public class PrincipalClinica extends JFrame {
 
 	/**
 	 * Create the frame.
-	 * @param acceso 
 	 */
-	public PrincipalClinica(Usuario acceso) {
-		setTitle("Cl\u00EDnica");
-		setResizable(false);
+	public PrincipalClinica() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 618, 416);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		mnNewMenu_1 = new JMenu("Citas");
-		menuBar.add(mnNewMenu_1);
-		
-		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Agendar Cita");
-		mnNewMenu_1.add(mntmNewMenuItem_3);
-		
-		mnNewMenu_2 = new JMenu("Usuarios");
-		menuBar.add(mnNewMenu_2);
-		
-		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Registrar Usuario");
-		mnNewMenu_2.add(mntmNewMenuItem_4);
-		
-		mnNewMenu_3 = new JMenu("Consultas");
-		menuBar.add(mnNewMenu_3);
-		
-		mnNewMenu_4 = new JMenu("Pacientes");
-		menuBar.add(mnNewMenu_4);
-		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Listado de Pacientes");
-		mnNewMenu_4.add(mntmNewMenuItem_1);
-		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Historial de Pacientes");
-		mnNewMenu_4.add(mntmNewMenuItem_2);
-		
-		mnNewMenu = new JMenu("Sesi\u00F3n");
-		menuBar.add(mnNewMenu);
-		
-		mntmNewMenuItem = new JMenuItem("Cambiar Usuario");
-		mntmNewMenuItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				AccesoSistema access = new AccesoSistema();
-				dispose();
-				access.setVisible(true);
-			}
-		});
-		mnNewMenu.add(mntmNewMenuItem);
+		setBounds(100, 100, 1150, 700);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		contentPane.add(panel, BorderLayout.SOUTH);
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 10));
+		panelMain = new JPanel();
+		panelMain.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.add(panelMain, BorderLayout.CENTER);
+		panelMain.setLayout(null);
 		
-		JLabel label = new JLabel("\u00A9Todos los derechos reservados. ");
-		label.setHorizontalAlignment(SwingConstants.LEFT);
-		label.setFont(new Font("Times New Roman", Font.ITALIC, 11));
-		panel.add(label);
+		panelUserInfo = new JPanel();
+		panelUserInfo.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelUserInfo.setBounds(0, 0, 1124, 46);
+		panelMain.add(panelUserInfo);
+		panelUserInfo.setLayout(null);
+		
+		btnLogOut = new JButton("Log In");
+		btnLogOut.setBounds(827, 13, 89, 23);
+		panelUserInfo.add(btnLogOut);
+		
+		JLabel lblNewLabel = new JLabel("User: N/A");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel.setBounds(10, 15, 166, 14);
+		panelUserInfo.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Inicio");
+		btnNewButton.setBounds(926, 13, 89, 23);
+		panelUserInfo.add(btnNewButton);
+		
+		btnNewButton_1 = new JButton("Cerrar");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnNewButton_1.setBounds(1025, 13, 89, 23);
+		panelUserInfo.add(btnNewButton_1);
+		
+		panelUser = new JPanel();
+		panelUser.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelUser.setBounds(0, 45, 1124, 21);
+		panelMain.add(panelUser);
+		panelUser.setLayout(null);
+		
+		menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 1124, 21);
+		panelUser.add(menuBar);
+		
+		mnAdministrador = new JMenu("Administrador");
+		menuBar.add(mnAdministrador);
+		
+		mntmRegMedico = new JMenuItem("Registrar Medico");
+		mnAdministrador.add(mntmRegMedico);
+		
+		mntmRegVacuna = new JMenuItem("Registrar Vacuna");
+		mnAdministrador.add(mntmRegVacuna);
+		
+		mntmRegEnfermedad = new JMenuItem("Registrar Enfermedad");
+		mnAdministrador.add(mntmRegEnfermedad);
+		
+		mnMedico = new JMenu("M\u00E9dico");
+		menuBar.add(mnMedico);
+		
+		mntmListCitaMedica = new JMenuItem("Lista Citas Medicas");
+		mnMedico.add(mntmListCitaMedica);
+		
+		mntmCrearConsulta = new JMenuItem("Hacer Consulta");
+		mnMedico.add(mntmCrearConsulta);
+		
+		mnVacunas = new JMenu("Vacunas");
+		menuBar.add(mnVacunas);
+		
+		mntmListVacuna = new JMenuItem("Lista de Vacunas");
+		mnVacunas.add(mntmListVacuna);
+		
+		mnListEnfermedades = new JMenu("Enfermedades");
+		menuBar.add(mnListEnfermedades);
+		
+		mntmNewMenuItem_6 = new JMenuItem("Lista de Enfermedades");
+		mnListEnfermedades.add(mntmNewMenuItem_6);
+		
+		panelFoundation = new JPanel();
+		panelFoundation.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelFoundation.setBounds(0, 67, 1124, 584);
+		panelMain.add(panelFoundation);
+		
+		panelLogin = new JPanel();
+		panelLogin.setBounds(0, 45, 1124, 606);
+		panelMain.add(panelLogin);
+	}
+	
+	private void guardarDatos() {
+		
 	}
 }
