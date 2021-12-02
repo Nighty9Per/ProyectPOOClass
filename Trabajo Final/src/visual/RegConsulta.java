6371730b3afec922ebe5191365f21438f4fa9d1a
reinoso <reinoso@DESKTOP-947B377>
2021-12-02 18:54:06 -0400

    Cambio en regConsulta
    -Ventana reestructurada

--- Trabajo Final/src/visual/RegConsulta.java
+++ Trabajo Final/src/visual/RegConsulta.java
@@ -26,16 +26,21 @@ import com.sun.org.apache.bcel.internal.classfile.ConstantUtf8;
 import javax.swing.JRadioButton;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Date;
 import java.awt.event.ActionEvent;
 import javax.swing.JList;
 import javax.swing.ListSelectionModel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.SimpleDateFormat;
+
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.JCheckBox;
 
 public class RegConsulta extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField txtCodigo;
 	private JTextPane txtpSintomas;
 	private JButton btnCancelar;
 	private JButton btnRegistro;
@@ -49,6 +54,10 @@ public class RegConsulta extends JDialog {
 	private ArrayList<String> Sickness;
 	private DefaultListModel<String> listModelEnfermedad;
 	private String selected = "";
+	private JLabel lblNewLabel;
+	private JLabel lblTlefono;
+	private JCheckBox chckbxPasarHistorial;
+	private static HistorialClinica hist = null;
 
 	/**
 	 * Launch the application.
@@ -67,10 +76,11 @@ public class RegConsulta extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegConsulta(Paciente patient, HistorialClinica history) {
+		hist = history;
 		setTitle("Registro de Consulta");
 		setResizable(false);
 		setModal(true);
-		setBounds(100, 100, 793, 733);
+		setBounds(100, 100, 793, 705);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -81,24 +91,12 @@ public class RegConsulta extends JDialog {
 			contentPanel.add(panel, BorderLayout.CENTER);
 			panel.setLayout(null);
 			
-			JLabel lblNewLabel = new JLabel("C\u00F3digo:");
-			lblNewLabel.setBounds(10, 11, 46, 14);
-			panel.add(lblNewLabel);
-			
-			txtCodigo = new JTextField();
-			txtCodigo.setEnabled(false);
-			txtCodigo.setEditable(false);
-			txtCodigo.setText("C-"+Clinica.getInstace().getGenerateCodigoConsulta());
-			txtCodigo.setColumns(10);
-			txtCodigo.setBounds(57, 8, 113, 23);
-			panel.add(txtCodigo);
-			
 			JLabel lblNewLabel_1 = new JLabel("S\u00EDntomas:");
-			lblNewLabel_1.setBounds(10, 60, 79, 14);
+			lblNewLabel_1.setBounds(10, 167, 79, 14);
 			panel.add(lblNewLabel_1);
 			
 			JPanel panel_1 = new JPanel();
-			panel_1.setBounds(10, 85, 220, 138);
+			panel_1.setBounds(10, 192, 220, 138);
 			panel.add(panel_1);
 			panel_1.setLayout(new BorderLayout(0, 0));
 			
@@ -109,11 +107,11 @@ public class RegConsulta extends JDialog {
 			scrollPane.setViewportView(txtpSintomas);
 			
 			JLabel lblNewLabel_2 = new JLabel("Diagn\u00F3stico:");
-			lblNewLabel_2.setBounds(10, 233, 90, 14);
+			lblNewLabel_2.setBounds(254, 167, 90, 14);
 			panel.add(lblNewLabel_2);
 			
 			JPanel panel_2 = new JPanel();
-			panel_2.setBounds(270, 85, 220, 138);
+			panel_2.setBounds(502, 192, 220, 138);
 			panel.add(panel_2);
 			panel_2.setLayout(new BorderLayout(0, 0));
 			
@@ -124,11 +122,11 @@ public class RegConsulta extends JDialog {
 			scrollPane_1.setViewportView(txtpProced);
 			
 			JLabel lblProcedimiento = new JLabel("Procedimiento:");
-			lblProcedimiento.setBounds(270, 60, 90, 14);
+			lblProcedimiento.setBounds(502, 167, 90, 14);
 			panel.add(lblProcedimiento);
 			
 			JPanel panel_3 = new JPanel();
-			panel_3.setBounds(534, 85, 220, 138);
+			panel_3.setBounds(254, 412, 220, 138);
 			panel.add(panel_3);
 			panel_3.setLayout(new BorderLayout(0, 0));
 			
@@ -139,11 +137,11 @@ public class RegConsulta extends JDialog {
 			scrollPane_2.setViewportView(txtpComent);
 			
 			JLabel lblTratamiento = new JLabel("Comentarios:");
-			lblTratamiento.setBounds(534, 60, 90, 14);
+			lblTratamiento.setBounds(254, 387, 90, 14);
 			panel.add(lblTratamiento);
 			
 			JPanel panel_4 = new JPanel();
-			panel_4.setBounds(10, 258, 350, 138);
+			panel_4.setBounds(254, 192, 220, 138);
 			panel.add(panel_4);
 			panel_4.setLayout(new BorderLayout(0, 0));
 			
@@ -154,11 +152,11 @@ public class RegConsulta extends JDialog {
 			scrollPane_3.setViewportView(txtpDiag);
 			
 			JLabel label = new JLabel("Tratamiento:");
-			label.setBounds(404, 234, 90, 14);
+			label.setBounds(10, 387, 220, 14);
 			panel.add(label);
 			
 			JPanel panel_5 = new JPanel();
-			panel_5.setBounds(404, 259, 350, 138);
+			panel_5.setBounds(10, 412, 220, 138);
 			panel.add(panel_5);
 			panel_5.setLayout(new BorderLayout(0, 0));
 			
@@ -169,7 +167,7 @@ public class RegConsulta extends JDialog {
 			scrollPane_4.setViewportView(txtpTratamiento);
 			
 			JLabel lblNewLabel_3 = new JLabel("\u00BFPosee el paciente un enfermedad en la lista de vigilancia?");
-			lblNewLabel_3.setBounds(10, 438, 432, 14);
+			lblNewLabel_3.setBounds(479, 349, 432, 14);
 			panel.add(lblNewLabel_3);
 			
 			rdbtnAfirmar = new JRadioButton("Si");
@@ -177,9 +175,10 @@ public class RegConsulta extends JDialog {
 				public void actionPerformed(ActionEvent e) {
 					rdbtnNegar.setSelected(false);
 					listEnfermedad.setEnabled(true);
+					chckbxPasarHistorial.setEnabled(false);
 				}
 			});
-			rdbtnAfirmar.setBounds(57, 459, 46, 23);
+			rdbtnAfirmar.setBounds(549, 370, 46, 23);
 			panel.add(rdbtnAfirmar);
 			
 			rdbtnNegar = new JRadioButton("No");
@@ -188,14 +187,16 @@ public class RegConsulta extends JDialog {
 					rdbtnAfirmar.setSelected(false);
 					listEnfermedad.setEnabled(false);
 					listEnfermedad.setSelectedIndex(0);
+					chckbxPasarHistorial.setSelected(false);
+					chckbxPasarHistorial.setEnabled(true);
 				}
 			});
 			rdbtnNegar.setSelected(true);
-			rdbtnNegar.setBounds(10, 459, 46, 23);
+			rdbtnNegar.setBounds(502, 370, 46, 23);
 			panel.add(rdbtnNegar);
 			
 			JPanel panel_6 = new JPanel();
-			panel_6.setBounds(10, 496, 160, 143);
+			panel_6.setBounds(502, 407, 220, 143);
 			panel.add(panel_6);
 			panel_6.setLayout(new BorderLayout(0, 0));
 			
@@ -219,6 +220,40 @@ public class RegConsulta extends JDialog {
 			listEnfermedad.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 			scrollPane_5.setViewportView(listEnfermedad);
 			
+			JPanel panel_7 = new JPanel();
+			panel_7.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Paciente: " +patient.getNombre(), TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+			panel_7.setBounds(10, 11, 745, 123);
+			panel.add(panel_7);
+			panel_7.setLayout(null);
+			
+			lblNewLabel = new JLabel("C\u00E9dula: "+patient.getCedula());
+			lblNewLabel.setBounds(10, 31, 160, 14);
+			panel_7.add(lblNewLabel);
+			
+			lblTlefono = new JLabel("T\u00E9lefono: "+patient.getTelefono());
+			lblTlefono.setBounds(167, 31, 138, 14);
+			panel_7.add(lblTlefono);
+			
+			
+			JLabel lblNewLabel_4 = new JLabel("Sexo: "+patient.getGenero());
+			lblNewLabel_4.setBounds(348, 31, 112, 14);
+			panel_7.add(lblNewLabel_4);
+			
+			Date dateAux = (Date) patient.getFechaNacimiento();
+			SimpleDateFormat de = new SimpleDateFormat("dd/MM/yyyy");
+			String year = de.format(dateAux);
+			JLabel lblNewLabel_5 = new JLabel("Fecha de Nacimiento: " +year);
+			lblNewLabel_5.setBounds(492, 31, 207, 14);
+			panel_7.add(lblNewLabel_5);
+			
+			JLabel lblNewLabel_6 = new JLabel("Direccion: " +patient.getDireccion());
+			lblNewLabel_6.setBounds(10, 74, 160, 14);
+			panel_7.add(lblNewLabel_6);
+			
+			chckbxPasarHistorial = new JCheckBox("Pasar Historial");
+			chckbxPasarHistorial.setBounds(10, 588, 121, 23);
+			panel.add(chckbxPasarHistorial);
+			
 			
 		}
 		{
@@ -230,13 +265,17 @@ public class RegConsulta extends JDialog {
 				btnRegistro = new JButton("Registrar");
 				btnRegistro.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
+						Consulta aux = null;
 						if (rdbtnNegar.isSelected()) {
-							Consulta aux =  Clinica.getInstace().crearConsulta(txtpSintomas.getText(), txtpDiag.getText(), txtpProced.getText(), txtpTratamiento.getText(), txtpComent.getText(), null);
+							aux =  Clinica.getInstace().crearConsulta(txtpSintomas.getText(), txtpDiag.getText(), txtpProced.getText(), txtpTratamiento.getText(), txtpComent.getText(), null);
 		
 						}else if (rdbtnAfirmar.isSelected()) {
 							String codigo = selected.substring(0, selected.lastIndexOf(":"));
 							Enfermedad sick = Clinica.getInstace().buscarEnfermedadCodigo(codigo);
-							Consulta aux =  Clinica.getInstace().crearConsulta(txtpSintomas.getText(), txtpDiag.getText(), txtpProced.getText(), txtpTratamiento.getText(), txtpComent.getText(), sick);
+							aux =  Clinica.getInstace().crearConsulta(txtpSintomas.getText(), txtpDiag.getText(), txtpProced.getText(), txtpTratamiento.getText(), txtpComent.getText(), sick);
+						}
+						if (rdbtnAfirmar.isSelected() || chckbxPasarHistorial.isSelected()) {
+							hist.getMisConsultas().add(aux);
 						}
 					}
 				});
